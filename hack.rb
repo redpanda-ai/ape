2e63f55f49a3dec25789a6b65224c3e266ffc23e
J. A. Key <joeandrewkey@gmail.com>
2010-11-19 12:30:47 -0800

    better hacking

--- hack.rb
+++ hack.rb
@@ -3,6 +3,30 @@
 require 'dbi'
 require 'ostruct'
 
+def truncate_attributes()
+	dsn, login, passwd, rules, pfile = ARGV[0],ARGV[1],ARGV[2]
+	cmd = 'TRUNCATE TABLE LogShipping.dba_tools.product_candidate_attributes'
+	db = DBI.connect("dbi:ODBC:" + dsn, login, passwd)
+	db.do(cmd)
+	db.disconnect
+end
+
+def insert_to_attributes()
+	dsn, login, passwd, rules, = ARGV[0],ARGV[1],ARGV[2],ARGV[3]
+	puts "inserting attributes"
+	big_insert = ''
+	for k,v in $product_attributes do
+		big_insert += "INSERT INTO LogShipping.dba_tools." \
+		"product_candidate_attributes(vendor_id, row_id, attribute_type," \
+		"attribute_name, attribute_value) VALUES (" \
+		+ "'" + k.vid + "', "
+		+ "'" + k.row_id + "', "
+		+ "'" + k.attribute_type + "', "
+		+ "'" + k.attribute_name + "', "
+		+ "'" + v + "')\n"
+	end
+end
+
 def foo()
 	dsn, login, passwd, rules, pfile = ARGV[0],ARGV[1],ARGV[2],ARGV[3],ARGV[4]
 	big_insert = ''
@@ -58,86 +82,95 @@ def crunch_product_file(products_file)
 	return product_keys, product_vals, lines
 end
 
-def compose_tie_breaker(tiebreaker_locations,line,line_number,vendor_id_pos,
-	vendor_name_pos)
-	c, product_values = 0, line.strip.split("\t")
+def compose_tie_breaker(t_params)
+	c = 0
 	#compose the tiebreaker values, they are in the product_values
 	tiebreaker_value = ''
-	for location in tiebreaker_locations do
-		tiebreaker_value += product_values[location]
+	for location in t_params.locations do
+		tiebreaker_value += t_params.product_values[location]
 	end
 
-	vendor_id = product_values[vendor_id_pos]
-	vendor_name = product_values[vendor_name_pos]
+	vendor = OpenStruct.new
+	vendor.vid = t_params.product_values[t_params.vid_pos]
+	vendor.name = t_params.product_values[t_params.name_pos]
 
 	tc = OpenStruct.new
-	tc.vendor_id, tc.line_number = vendor_id, line_number
+	tc.vendor_id, tc.line_number = vendor.vid, t_params.line_number
 	tc.tiebreaker = tiebreaker_value
-	if $broken_ties.has_value?(tiebreaker_value)
+	#place the tiebreaker_candidate in one of two buckets
+	if $broken_ties.has_key?(tiebreaker_value)
+		#puts "unbroken tie"
 		$unbroken_ties << tc
 	else
+		#puts "broken tie"
 		$broken_ties[tiebreaker_value] = tc
+		#puts "broken_ties -> #{$broken_ties.length.to_s()}"
 	end
-	return vendor_id, vendor_name
+	return vendor
 end
 
-def map_products(products_file)
-	product_keys, product_vals, lines = crunch_product_file(products_file)
+def confirm_uniqueness(product_keys, product_vals, lines)
 
+	t_params = OpenStruct.new
+	t_params.vid_pos = product_keys["Vendor ID"]
+	t_params.name_pos = product_keys["Vendor Name"]
+	t_params.locations = []
 	t = $tiebreaker.split("+")
-	tiebreaker_locations = []
-	for item in t do tiebreaker_locations << product_keys[item.strip] end
-	vendor_id_pos = product_keys["Vendor ID"]
-	vendor_name_pos = product_keys["Vendor Name"]
+	for item in t do
+		t_params.locations << product_keys[item.strip]
+	end #end for
 
 	line_number, found_matching_rule = 0,0
+	#generate tie breaker for each product 
 	for line in lines do
-		c, product_values = 0, line.strip.split("\t")
-		vendor_id, vendor_name = compose_tie_breaker(tiebreaker_locations,line,
-		line_number,vendor_id_pos, vendor_name_pos)
-	#compose the tiebreaker values, they are in the product_values
-		#tiebreaker_value = ''
-		#for location in tiebreaker_locations do
-		#	tiebreaker_value += product_values[location]
-		#end
-
-		#vendor_id = product_values[vendor_id_pos]
-		#vendor_name = product_values[vendor_name_pos]
-
-		#tc = OpenStruct.new
-		#tc.vendor_id, tc.line_number = vendor_id, line_number
-		#tc.tiebreaker = tiebreaker_value
-		#if $broken_ties.has_value?(tiebreaker_value)
-		#	$unbroken_ties << tc
-		#else
-		#	$broken_ties[tiebreaker_value] = tc
-		#end
-
-		for item in product_values do
-			t = OpenStruct.new
-			t.vendor_name, t.vendor_id = vendor_id, vendor_name
-			t.field_name = product_vals[c]
-			found_matching_rule += 1
-			if $rules.has_value?(t)
-				found_matching_rule +=1
-			end
-			c += 1
-		end
+		t_params.product_values = line.strip.split("\t")
+		t_params.line_number = line_number
+		vendor = compose_tie_breaker(t_params)
 		line_number += 1
-	end
+		#check tie breaker against rules
+		c = 0
+		for item in t_params.product_values do
+			#construct a key that may be in $rules
+			t = OpenStruct.new
+			t.vendor_name, t.vendor_id = vendor.name, vendor.vid
+			t.field_name = product_vals[c]
+			#scan rules for key
+			for k,v in $rules do
+				if (k == t)
+					found_matching_rule +=1
+					x = OpenStruct.new
+					x.vid = vendor.vid
+					x.row_id = t_params.line_number
+					x.attribute_type = v.our_type
+					x.attribute_name = v.our_field_name
+					$product_attributes[x] = item
+				end #end if
+			end #end for
+			c += 1
+		end #end for
+	end #end for
 
 	if found_matching_rule == 0
 		puts "Matching rules not found, aborting"
 		exit
-	end
+	end #end if
 
 	if $unbroken_ties.length > 0
+		filter = {}
+		for item in $unbroken_ties do
+			filter[item.tiebreaker] = item.tiebreaker
+		end
+		for k,v in filter do
+			$unbroken_ties << $broken_ties.delete(k)
+		end
+			
 		puts "Your tiebreaker does not break all ties, please correct it"
-		puts "broken_ties -> " + $broken_ties.length.to_s()
-		puts "unbroken_ties -> " + $unbroken_ties.length.to_s()
+		puts "broken_ties (count) -> " + $broken_ties.length.to_s()
+		puts "unbroken_ties (count) -> " + $unbroken_ties.length.to_s()
 	else
 		puts "Your tiebreaker is successful."
-		puts "broken_ties -> " + $broken_ties.length.to_s()
+		puts "broken_ties (count) -> " + $broken_ties.length.to_s()
+		puts "unbroken_ties (count) -> " + $unbroken_ties.length.to_s()
 	end
 end
 
@@ -148,13 +181,30 @@ unless ARGV.length == 5
 end
 dsn, login, passwd, rfile, pfile = ARGV[0],ARGV[1],ARGV[2],ARGV[3],ARGV[4]
 
-#foo()
-$rules, $products = {}, {}
+$rules, $product_attributes = {}, {}
 $tiebreaker, $broken_ties, $unbroken_ties = '', {}, []
 make_rules(rfile)
-map_products(pfile)
+product_keys, product_vals, lines = crunch_product_file(pfile)
+confirm_uniqueness(product_keys, product_vals, lines)
+truncate_attributes()
+insert_to_attributes()
+#print "PRODUCTS -> #{$product_attributes.to_s()}"
+puts "product_attributes_count -> #{$product_attributes.length.to_s()}"
 
 #lines = File.open(products).collect
-
 #select = db.prepare('SELECT TOP 10 * FROM bcproduct.dbo.item')
 #puts "Number of rows inserted: #{rows}"
+		#check tie breaker against rules
+		#c = 0
+		#for item in t_params.product_values do
+			#t = OpenStruct.new
+			#t.vendor_name, t.vendor_id = vendor.vid, vendor.name
+			#t.field_name = product_vals[c]
+			#found_matching_rule += 1
+			#if $rules.has_value?(t)
+			#	found_matching_rule +=1
+			#end
+			#c += 1
+		#end
+
+
