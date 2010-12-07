82cfc4290f260d25f32219f374b5b09236b89455
J. A. Key <joeandrewkey@gmail.com>
2010-12-07 12:16:01 -0800

    fixed an error with parsing spaces

--- dedupe_symlink.sh
+++ dedupe_symlink.sh
@@ -2,32 +2,34 @@
 
 IFS=$'\n'
 k=$'\t'
+delim='\\ '
+sp=' '
 
 trim()
 {
-        trimmed=$1
-        trimmed=${trimmed%% }
-        trimmed=${trimmed## }
-
-        echo $trimmed
+	trimmed=$1
+	trimmed=${trimmed%% }
+	trimmed=${trimmed## }
+	trimmed=${trimmed//$delim/$sp}
+	echo $trimmed
 }
 for i in $(fdupes -r1 ./);
 do
-        h=${i//\.\//$k}
-        IFS=$k
-        count=0
-        for j in $h;
-        do
-                if [ $count -eq 0 ] ; then
-                        source=$j
-                else
-                        link=$(trim $j)
-                        actual=$(trim $source)
-                        rm ${link}
-                        ln -s $PWD/$actual $PWD/$link
-                fi
-                let "count += 1"
-        done
-        IFS=$'\n'
+	h=${i//\.\//$k}
+	IFS=$k
+	count=0
+	for j in $h;
+	do
+		if [ $count -eq 0 ] ; then
+			actual=$(trim $j)
+		else
+			link=$(trim $j)
+			echo rm "${link}"
+			rm "${link}"
+			echo ln -s $PWD/$actual $PWD/$link
+			ln -s $PWD/$actual $PWD/$link
+		fi
+		let "count += 1"
+	done
+	IFS=$'\n'
 done
-
